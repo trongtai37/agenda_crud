2adfee9853f5a72f461f4a752d5497801c823975
Tai Nguyen Trong <dongthoigian@gmail.com>
2022-06-26 09:44:47 +0700

    fix: transform list query on backend

--- backend/src/agenda/dto/get-list-query.ts
+++ backend/src/agenda/dto/get-list-query.ts
@@ -1,11 +1,14 @@
+import { Transform } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
 export class GetAgendaQuery {
   @IsNumber()
   @IsOptional()
+  @Transform(({ value }) => Number(value))
   page?: number;
 
   @IsNumber()
   @IsOptional()
+  @Transform(({ value }) => Number(value))
   perPage?: number;
 }
