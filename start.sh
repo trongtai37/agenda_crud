ddeb5f586947a3616b0ac019e401e184aa0cc55e
Tai Nguyen Trong <dongthoigian37@gmail.com>
2022-06-26 17:48:36 +0700

    fix: correct start.sh file

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 # Setup backend
 cd backend
 npm install 
-npm run start:dev
 
 # Setup frontend
 cd ../frontend
 npm install
-npm start
+
+cd ../
+(cd ./backend && npm run start:dev) & (cd ./frontend && npm start)
